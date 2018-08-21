e3a76bcb25f4f0647b2b750f5a795c18f3145bae
coryellenberger <cory.ellen.ce@gmail.com>
2018-08-20 22:15:04 -0700

    [Package Rename/Migration from Apollo Boost] - api/package.json ~ updating the name to objective-dash-api ~ updating the description
    - ui/package.json
    ~ updating the name to objective-dash-ui
    ~ updating the description
    ~ migrating from apollo-boost to the actual apollo libraries
    
    - index.js
    ~ updating to use the proper Apollo libraries to setup the server
    ~ this will be required in order to have more control over the environment and configuration for authentication and other things

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 
 const client = new ApolloClient({
-    uri: process.env.REACT_APP_GRAPHQL_URI
-})
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: process.env.REACT_APP_GRAPHQL_URI,
+      credentials: 'same-origin'
+    })
+  ]),
+  cache: new InMemoryCache()
+});
 
 const Main = () => (
     <ApolloProvider client={client}>
