d12cacc6459fe31d293ad4703478592f20bb0d66
coryellenberger <cory.ellen.ce@gmail.com>
2018-08-21 01:37:50 -0700

    [Login Functionality] - graphql-schema.js ~ setting up a lot of stuff for the login on the backend of the application ~ setting up backend signup/login and check the current user logged in with header
    - .env
    ~ adding a default JWT_SECRET
    
    - api/index.js
    ~ Working out the authentication layer/logic with context
    
    - index.js
    ~ updating indentation
    
    fixes#1

--- api/src/graphql-schema.js
+++ api/src/graphql-schema.js
@@ -1,53 +1,165 @@
-import { neo4jgraphql } from "neo4j-graphql-js";
+import {v1 as neo4j} from "neo4j-driver";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+let driver;
 
 export const typeDefs = `
-type User {
-  id: ID!
-  name: String
-  friends(first: Int = 10, offset: Int = 0): [User] @relation(name: "FRIENDS", direction: "BOTH")
-  reviews(first: Int = 10, offset: Int = 0): [Review] @relation(name: "WROTE", direction: "OUT")
-  avgStars: Float @cypher(statement: "MATCH (this)-[:WROTE]->(r:Review) RETURN toFloat(avg(r.stars))")
-  numReviews: Int @cypher(statement: "MATCH (this)-[:WROTE]->(r:Review) RETURN COUNT(r)")
-}
-
-type Business {
-  id: ID!
-  name: String
-  address: String
-  city: String
-  state: String
-  reviews(first: Int = 10, offset: Int = 0): [Review] @relation(name: "REVIEWS", direction: "IN")
-  categories(first: Int = 10, offset: Int =0): [Category] @relation(name: "IN_CATEGORY", direction: "OUT")
-}
-
-type Review {
-  id: ID!
-  stars: Int
-  text: String
-  business: Business @relation(name: "REVIEWS", direction: "OUT")
-  user: User @relation(name: "WROTE", direction: "IN")
-}
-
-type Category {
-  name: ID!
-  businesses(first: Int = 10, offset: Int = 0): [Business] @relation(name: "IN_CATEGORY", direction: "IN")
-}
-
-type Query {
-    users(id: ID, name: String, first: Int = 10, offset: Int = 0): [User]
-    businesses(id: ID, name: String, first: Int = 10, offset: Int = 0): [Business]
-    reviews(id: ID, stars: Int, first: Int = 10, offset: Int = 0): [Review]
-    category(name: ID!): Category
-    usersBySubstring(substring: String, first: Int = 10, offset: Int = 0): [User] @cypher(statement: "MATCH (u:User) WHERE u.name CONTAINS $substring RETURN u")
-}
+  type User {
+    id: ID!
+    email: String!
+    password: String!
+    jwt: String
+  }
+  
+  type Query {
+    currentUser: User
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): User
+    signup(email: String!, password: String!): User
+  }
 `;
 
 export const resolvers = {
   Query: {
-    users: neo4jgraphql,
-    businesses: neo4jgraphql,
-    reviews: neo4jgraphql,
-    category: neo4jgraphql,
-    usersBySubstring: neo4jgraphql
+    currentUser: async (root, args, scope) => {
+      const context = await scope.authScope;
+      return await context.user;
+    },
+  },
+  Mutation: {
+    login: async (root, { email, password }, scope) => {
+      const context = await scope.authScope;
+
+      const session = context.driver.session();
+
+      const userResults = await session
+        .run('MATCH (user:User { email: { email } }) RETURN user', { email: email });
+
+      const user = userResults.records[0].get('user').properties
+
+      if (!user) {
+        throw new Error('Email not found');
+      }
+
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (!validPassword) {
+        throw new Error('Password is incorrect');
+      }
+
+      user.jwt = jwt.sign({ id: user.id }, context.secrets.JWT_SECRET);
+
+      session.close();
+
+      return user;
+    },
+    signup: async (root, { email, password }, scope) => {
+      const context = await scope.authScope;
+
+      const session = context.driver.session();
+
+      const existingUser = await session
+        .run('MATCH (user:User { email: { email } }) RETURN user', { email: email });
+
+      if (existingUser.records.length > 0) {
+        throw new Error('Email already used');
+      }
+
+      const hash = await bcrypt.hash(password, 10);
+      const numberUsers = await session
+        .run('MATCH (users:User) RETURN count(*)');
+      const count = parseInt(numberUsers.records[0].get('count(*)')) + 1
+      const results = await session
+        .run('CREATE (newUser:User { id: { id }, email: { email }, password: { password } }) RETURN newUser', {
+          id: 'u' + count,
+          email: email,
+          password: hash
+        });
+      const user = results.records[0].get('newUser').properties;
+
+      user.jwt = jwt.sign({ id: user.id }, context.secrets.JWT_SECRET);
+
+      session.close();
+
+      return user;
+    }
   }
 };
+
+// https://launchpad.graphql.com/n4xk8xm87
+const getUser = async (authorization, secrets, driver) => {
+  const bearerLength = "Bearer ".length;
+  if (authorization && authorization.length > bearerLength) {
+    const token = authorization.slice(bearerLength);
+    const { ok, result } = await new Promise(resolve =>
+      jwt.verify(token, secrets.JWT_SECRET, (err, result) => {
+        if (err) {
+          console.log('Ok error');
+          resolve({
+            ok: false,
+            result: err
+          });
+        } else {
+          console.log('Ok 2');
+          resolve({
+            ok: true,
+            result
+          });
+        }
+      })
+    );
+
+    if (ok) {
+      const session = driver.session();
+
+      console.log(result.id);
+
+      const results = await session
+        .run('MATCH (user:User { id: { id } }) RETURN user', { id: result.id });
+
+      const user = results.records[0].get('user').properties;
+
+      console.log(user);
+
+      session.close();
+      return user;
+    } else {
+      console.error(result);
+      return null;
+    }
+  }
+
+  return null;
+};
+
+export async function context(req) {
+  if (!driver) {
+    driver = neo4j.driver(
+      process.env.NEO4J_URI || "bolt://localhost:7687",
+      neo4j.auth.basic(
+        process.env.NEO4J_USER || "neo4j",
+        process.env.NEO4J_PASSWORD || "neo4j"
+      )
+    );
+  }
+
+  let secrets;
+
+  if (!secrets) {
+    secrets = process.env;
+  }
+
+  let headers = req.headers;
+
+  const user = await getUser(headers.authorization, secrets, driver);
+
+  return {
+    headers,
+    secrets,
+    driver,
+    user,
+  };
+
+}
--- ui/src/index.js
+++ ui/src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {onError} from 'apollo-link-error';
+import {ApolloLink} from 'apollo-link';
 
+// Create the Apollo Client with configuration
 const client = new ApolloClient({
+  // Configure Link for GraphQL
   link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
+    onError(({graphQLErrors, networkError}) => {
       if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
+        graphQLErrors.map(({message, locations, path}) =>
           console.log(
             `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
           ),
@@ -30,10 +32,10 @@ const client = new ApolloClient({
 });
 
 const Main = () => (
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
 )
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(<Main/>, document.getElementById('root'));
 registerServiceWorker();
