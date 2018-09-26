d087f143b82d6bd85ba4f58c3fd35d34354e7a73
coryellenberger <cory.ellen.ce@gmail.com>
2018-09-25 22:32:22 -0700

    [Resolver Middleware] - index.js ~ setting up the schema level resolver which will be our authentication middleware ~ we will now check if the route is private - if so we will confirm the user is authenticated ~ otherwise we will throw the user an error before allowing through to the private resolver
    - graphql-schema.js
    ~ adding jsdocs for the current API's that exist for signup login and getting the current user authenticated
    ~ adding back the logic to add the jwt and expirationDate to the user returned when signing up (for now)
    ~ adding array of publicResolvers that will be used to determine which resolvers we will authenticate
    
    #fixed #13

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,5 @@
-import {typeDefs, resolvers, context} from "./graphql-schema";
-import {ApolloServer} from "apollo-server";
+import {typeDefs, resolvers, publicResolvers, context} from "./graphql-schema";
+import {ApolloServer, AuthenticationError} from "apollo-server";
 import dotenv from "dotenv";
 
 import {makeExecutableSchema, addSchemaLevelResolveFunction} from 'graphql-tools';
@@ -12,9 +12,17 @@ if (typeof process.env.JWT_SECRET === 'undefined') {
 
 const schema = makeExecutableSchema({typeDefs, resolvers});
 
-addSchemaLevelResolveFunction(schema, (root, args, context, info) => {
-  console.log('hitting the schema resolver');
-  console.log('info', info);
+addSchemaLevelResolveFunction(schema, async (root, args, context, info) => {
+  // if Private Resolver is being handled
+  if (publicResolvers.indexOf(info.fieldName) === -1) {
+    // gather current user from the context
+    const scope = await context.authScope;
+    const user = await scope.user;
+    if (!user) {
+      console.error('User NOT authenticated attempted to hit API:', info.fieldName);
+      throw new AuthenticationError('Not Authenticated');
+    }
+  }
 });
 
 const server = new ApolloServer({
